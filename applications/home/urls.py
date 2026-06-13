d5be9531aa0744517e8dabb9d925b6d451b48ac0
unknown <roberthbardales@hotmail.com>
2026-06-12 19:21:28 -0500

    templates de home

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'app_home'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('nosotros/', views.AboutView.as_view(), name='about'),
+    path('productos/', views.ProductsView.as_view(), name='products'),
+    path('categorias/', views.CategoriesView.as_view(), name='categories'),
+    path('marcas/', views.BrandsView.as_view(), name='brands'),
+    path('garantia/', views.WarrantyView.as_view(), name='warranty'),
+    path('contacto/', views.ContactView.as_view(), name='contact'),
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -3,3 +3,27 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'home/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+
+class ProductsView(TemplateView):
+    template_name = 'home/products.html'
+
+
+class CategoriesView(TemplateView):
+    template_name = 'home/categories.html'
+
+
+class BrandsView(TemplateView):
+    template_name = 'home/brands.html'
+
+
+class WarrantyView(TemplateView):
+    template_name = 'home/warranty.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
